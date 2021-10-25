0069e4a56a4155748611804ae6cdf42c9f472073
Oran C <oranbusiness@gmail.com>
2021-10-24 19:01:57 -0700

    [add] ui buttons

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
-
+// let example_text = `function x() {
+//   console.log("Hello world!");
+// }` 
 // require is provided by loader.min.js.
-require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs' }});
+require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs' } });
 require(["vs/editor/editor.main"], () => {
-  monaco.editor.create(document.getElementById('container'), {
-    value: `function x() {
-  console.log("Hello world!");
-}`,
+  var editor = monaco.editor.create(
+    document.getElementById('container'), {
+    value: example_text,
     language: 'javascript',
     theme: 'vs-dark',
+    language: "javascript",
+
+    wordWrap: 'wordWrapColumn',
+    wordWrapColumn: 40,
+
+    // Set this to false to not auto word wrap minified files
+    wordWrapMinified: true,
+
+    // try "same", "indent" or "none"
+    wrappingIndent: "indent"
   });
+
+  
+var myBinding = editor.addCommand(monaco.KeyCode.F9, function() {
+	alert('F9 pressed!');
 });
+});
+
